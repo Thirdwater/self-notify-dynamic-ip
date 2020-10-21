5493361359af5bdb5db683674ad78b8562197594
Thirdwater <irinthirdwater@gmail.com>
2020-10-21 05:14:56 +0100

    Use smtp directly instead of the gmail api

--- script.py
+++ script.py
@@ -1,23 +1,26 @@
+#!/usr/bin/env python3
+
 import os
 import json
+from dotenv import load_dotenv
+
 import requests
+import ssl
+from smtplib import SMTP_SSL
+from email.message import EmailMessage
 
 
 def main():
-    filename = "last-ip.json"
-    last_ip = get_last_ip(filename)
+    load_dotenv()
+    last_ip = get_last_ip()
     current_ip = get_external_ip()
-    print(last_ip)
-    print(current_ip)
     if current_ip != last_ip:
-        print("New IP")
-
-        save_ip(current_ip, filename)
-    else:
-        print("Same IP")    
+        send_email(current_ip)
+        save_ip(current_ip)
 
 
-def get_last_ip(filename:str) -> str:
+def get_last_ip() -> str:
+    filename = os.getenv('LAST_IP_FILE')
     if not os.path.isfile(filename):
         return "0.0.0.0"
     with open(filename) as json_file:
@@ -25,7 +28,8 @@ def get_last_ip(filename:str) -> str:
         return data['last_ip']
 
 
-def save_ip(ip:str, filename:str):
+def save_ip(ip:str):
+    filename = os.getenv('LAST_IP_FILE')
     with open(filename, 'w') as json_file:
         json.dump({'last_ip': ip}, json_file)
 
@@ -33,6 +37,21 @@ def save_ip(ip:str, filename:str):
 def get_external_ip() -> str:
     # See https://stackoverflow.com/a/36205547
     return requests.get("https://api.ipify.org").text
+
+
+def send_email(ip:str):
+    message = EmailMessage()
+    message['Subject'] = os.getenv('EMAIL_SUBJECT')
+    message['From'] = os.getenv('SENDER_EMAIL')
+    message['To'] = os.getenv('RECEIVER_EMAIL')
+    message.set_content(ip)
+    sender_password = os.getenv('SENDER_PASSWORD')
+    port = int(os.getenv('GMAIL_SMTP_SSL_PORT'))
+    
+    context = ssl.create_default_context()
+    with SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(message['From'], sender_password)
+        server.send_message(message)
 
 
 if __name__ == '__main__':
